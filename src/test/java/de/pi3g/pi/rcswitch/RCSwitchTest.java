f2422260b03e0272e86796415534503219a89ff0
karim.bejaoui <karim.bejaoui@gmail.com>
2015-01-18 12:24:49 +0100

    Ignore Junit Test

--- src/test/java/de/pi3g/pi/rcswitch/RCSwitchTest.java
+++ src/test/java/de/pi3g/pi/rcswitch/RCSwitchTest.java
@@ -1,14 +1,20 @@
 package de.pi3g.pi.rcswitch;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import com.pi4j.io.gpio.RaspiPin;
 
+/**
+ * Remove @Ignore and change GPIO Number to launch test
+ *  
+ */
 public class RCSwitchTest {
 
 	RCSwitch transmitter = new RCSwitch(RaspiPin.GPIO_00);
 
 	@Test
+	@Ignore
 	public void testLoop() throws InterruptedException {
 
 		while (true) {
